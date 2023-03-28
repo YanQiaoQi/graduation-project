c69790414617502acc2f6ff996180d99fafe44b1
宏缘 <1981405879@qq.com>
2023-03-29 01:02:42 +0800

    feat:test

--- graduation-project-network/chaincode/test/index.js
+++ graduation-project-network/chaincode/test/index.js
@@ -1,30 +1,4 @@
-const shim = require("fabric-shim")
+'use strict';
 
-export const chaincode = {
-	async Init(stub) {
-		return shim.success(
-			Buffer.from("Initialized Successfully!")
-		);
-	},
-
-	async Invoke(stub) {
-		let {
-			fcn,
-			params
-		} = stub.getFunctionAndParameters();
-		// @ts-ignore
-		const method = this?.[fcn];
-		if (!method) {
-			console.shim.error("未找到", fcn, "函数");
-		}
-		try {
-			const payload = await method(stub, params);
-			return shim.success(Buffer.from(payload));
-		} catch (e) {
-			console.log(e);
-			return shim.error(Buffer.from(String(e)));
-		}
-	},
-};
-
-shim.start(chaincode);
+const UpdateValues = require('./updatevalues')
+module.exports.contracts = ['UpdateValues'];
